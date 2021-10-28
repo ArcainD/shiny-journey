f94c8d97b9e9752891ee9c4018466ff9b6189ab5
Ilya Litvin <ltvin@yandex.ru>
2021-10-28 23:08:25 +0300

    case 1 fixed

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ with open('cook book.txt', encoding='utf-8') as file:
         temp = []
         for item in range(counter):
             ingredient_name, quantity, measure = file.readline().split('|')
-            temp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-        res[dish] = temp
+            temp.append({'ingredient_name': ingredient_name.strip(), 'quantity': int(quantity.strip()), 'measure': measure.strip()})
+        res[dish_name] = temp
         file.readline()
-    pprint(res)
+pprint(res)
+
+
+# def get_shop_list_by_dishes(dishes):
+     # result ={}
+     # for dish in res.keys():
+     #     return dish
+#          if dish == dishes:
+#              return dish.values()
+# print(get_shop_list_by_dishes('Омлет'))
+# print(get_shop_list_by_dishes('Омлет'))
+
