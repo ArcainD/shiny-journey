0285411d1a85b28972d4f079e8e70aaeb1a50670
Ilya Litvin <ltvin@yandex.ru>
2021-10-29 22:13:58 +0300

    case 2 filepath fixed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pprint import pprint
+import os
 
-with open('cook book.txt', encoding='utf-8') as file:
+path = f'{os.getcwd()}\\recipes.txt'
+
+with open(path, encoding='UTF-8') as file:
     res = {}
     for dish in file:
         dish_name = dish.strip()
@@ -40,7 +43,59 @@ def get_shop_list_by_dishes(dishes, person_count):
 pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
 
 
-
-
-
-
+# def rewrite_file():
+#     outout_file = "rewrite_file.txt"
+#     with open('1.txt', 'r', encoding='utf-8') as f1:
+#         file1 = f1.readlines()
+#     with open('2.txt', 'r', encoding='utf-8') as f2:
+#         file2 = f2.readlines()
+#     with open('3.txt', 'r', encoding='utf-8') as f3:
+#         file3 = f3.readlines()
+#     with open(outout_file, 'w', encoding='utf-8') as f_total:
+#         if len(file1) < len(file2) and len(file1) < len(file3):
+#             f_total.write(path1 + '\n')
+#             f_total.write(str(len(file1)) + '\n')
+#             f_total.writelines(file1)
+#             f_total.write('\n')
+#         elif len(file2) < len(file1) and len(file2) < len(file3):
+#             f_total.write(path2 + '\n')
+#             f_total.write(str(len(file2)) + '\n')
+#             f_total.writelines(file2)
+#             f_total.write('\n')
+#         elif len(file3) < len(file1) and len(file3) < len(file2):
+#             f_total.write(path3 + '\n')
+#             f_total.write(str(len(file3)) + '\n')
+#             f_total.writelines(file3)
+#             f_total.write('\n')
+#         if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(                    file3):
+#             f_total.write(path1 + '\n')
+#             f_total.write(str(len(file1)) + '\n')
+#             f_total.writelines(file1)
+#             f_total.write('\n')
+#         elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) < len(
+#                 file3):
+#             f_total.write(path2 + '\n')
+#             f_total.write(str(len(file2)) + '\n')
+#             f_total.writelines(file2)
+#             f_total.write('\n')
+#         elif len(file1) > len(file3) > len(file2) or len(file3) > len(file1) and len(file3) < len(
+#                 file2):
+#             f_total.write(path3 + '\n')
+#             f_total.write(str(len(file3)) + '\n')
+#             f_total.writelines(file3)
+#             f_total.write('\n')
+#         if len(file1) > len(file2) and len(file1) > len(file3):
+#             f_total.write(path1 + '\n')
+#             f_total.write(str(len(file1)) + '\n')
+#             f_total.writelines(file1)
+#         elif len(file2) > len(file1) and len(file2) > len(file3):
+#             f_total.write(path2 + '\n')
+#             f_total.write(str(len(file2)) + '\n')
+#             f_total.writelines(file2)
+#         elif len(file3) > len(file1) and len(file3) > len(file2):
+#             f_total.write(path3 + '\n')
+#             f_total.write(str(len(file3)) + '\n')
+#             f_total.writelines(file3)
+#         else:
+#             print('hi')
+#     return
